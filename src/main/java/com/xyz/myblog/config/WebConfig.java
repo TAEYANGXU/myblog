6ec3e4c3493fe4ac0131f2b51723b8410761eb6c
xuyanzhang <albert_xyz@163.com>
2025-07-15 14:44:17 +0800

    Header参数校验

---
+++ src/main/java/com/xyz/myblog/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.xyz.myblog.config;
+
+import com.xyz.myblog.interceptor.JwtInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private JwtInterceptor headerInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(headerInterceptor)
+                .addPathPatterns("/**")
+                // .excludePathPatterns("/api/register/**")
+                .excludePathPatterns("/api/login"); // 放行登录接口
+    }
+}
--- src/main/java/com/xyz/myblog/service/GlobalResponseAdvice.java
+++ src/main/java/com/xyz/myblog/service/GlobalResponseAdvice.java
@@ -5,7 +5,6 @@ import com.xyz.myblog.dto.response.ApiResponse;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
